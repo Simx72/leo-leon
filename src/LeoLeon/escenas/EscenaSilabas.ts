2cf4445e22a4869731f12345ad651c79807c6453
Simx72 <angel2600@proton.me>
2024-02-08 11:25:49 -0500

    siuuu

--- src/LeoLeon/escenas/EscenaSilabas.ts
+++ src/LeoLeon/escenas/EscenaSilabas.ts
@@ -1,6 +1,7 @@
 import { Escena } from "./Escena";
 import html from "./EscenaSilabas.html?url";
 import letrasJson from '../../assets/json/letras.json';
+import 'animate.css/animate.min.css';
 
 export class EscenaSilabas extends Escena {
     async cargar(): Promise<void> {
@@ -67,20 +68,17 @@ export class EscenaSilabas extends Escena {
         this.letras.forEach(letra => {
             letra.addEventListener('mousedown', ev => {
                 ev.preventDefault();
-                // let {clientLeft: x, clientTop: y} = ev.target as HTMLDivElement;
-                console.log('mousedown');
+                // console.log('mousedown');
                 this.nuevoBloqueFlotante(ev.target as HTMLDivElement);
 
             })
-            // letra.addEventListener('mouseleave', ev=>{
-            //     ev.preventDefault();
-            //     // let {clientLeft: x, clientTop: y} = ev.target as HTMLDivElement;
-            //     console.log('leave');
-            // })
         })
         window.addEventListener('mousemove', ev => {
             this.mouseX = ev.clientX;
             this.mouseY = ev.clientY;
+        })
+        window.addEventListener('mouseup', _ev => {
+            this.seleccionado = null;
         })
     }
 
@@ -88,6 +86,7 @@ export class EscenaSilabas extends Escena {
     mouseY = 0;
 
     nuevoBloqueFlotante(desde: HTMLDivElement) {
+        // encontrar la letra
         let infoLetra: {
             id: null | string,
             vocal: boolean
@@ -95,40 +94,69 @@ export class EscenaSilabas extends Escena {
             id: null,
             vocal: false
         }
-
         infoLetra.id = desde.getAttribute('data-letra');
         infoLetra.vocal = Boolean(desde.getAttribute('data-vocal'));
-
         let letra = letrasJson.find(l => l.LETRA == infoLetra.id);
 
+        // verificarla
         if (letra) {
+            // crearla
             console.log('creando nuevo bloque', letra);
 
             let nueva = document.createElement('div');
-            nueva.classList.add('bloque', 'flotante');
+            nueva.classList.add('bloque', 'flotante', 'animate__bounceIn');
             let text = document.createTextNode(letra.MINUSCULA);
             nueva.appendChild(text);
-            this.seleccionado = nueva;
             if (letra.VOCAL) {
                 nueva.classList.add('vocal');
                 nueva.setAttribute('vocal', 'true');
-                this.zonaLF.appendChild(nueva);
             } else {
                 nueva.classList.add('conso');
-                this.zonaLF.appendChild(nueva);
                 nueva.setAttribute('vocal', 'false');
             }
+
+            // añadirla al DOM
+            this.zonaLF.appendChild(nueva);
+
+            // seleccionarla: una letra seleccionada se mueve con el cursor
+            this.seleccionado = nueva;
+            
+            // añadirla a las letras existentes
             this.letrasFlotantes.push(nueva);
+
+            // añadir eventos
+            nueva.addEventListener('mousedown', _ev => {
+                if (!this.oprimiendoEliminar) {
+                    this.seleccionado = nueva;
+                }
+            })
+
+            // añadir boton de eliminar
+            this.nuevoBotonEliminar(nueva);
+            
         } else {
             console.log('no encontramos ninguna letra ', infoLetra);
         }
     }
 
-    seleccionado: null | HTMLDivElement = null;
+    _seleccionado: null | HTMLDivElement = null;
+    get seleccionado (): null | HTMLDivElement {
+        return this._seleccionado;
+    }
+    set seleccionado(elemento: null | HTMLDivElement) {
+        if (this._seleccionado) {
+            this._seleccionado.setAttribute('data-agarra', 'false');
+        }
+        if (elemento) {
+            elemento.setAttribute('data-agarra', 'true');
+        }
+        this._seleccionado = elemento;
+    }
+
     moviendo = false;
     mover(element: HTMLDivElement, x: number, y: number) {
-        element.style.left = x + "px";
-        element.style.top = y + "px";
+        element.style.left = (x - 30) + "px";
+        element.style.top = (y - 30) + "px";
     }
 
     actualizar(_delta: number): void {
@@ -136,4 +164,47 @@ export class EscenaSilabas extends Escena {
             this.mover(this.seleccionado, this.mouseX, this.mouseY);
         }
     }
+
+    oprimiendoEliminar = false;
+    nuevoBotonEliminar (letra: HTMLDivElement) {
+        // crear boton
+        let btn = document.createElement('button');
+        
+        // configurar boton
+        btn.classList.add('boton-eliminar');
+        btn.innerText = 'X';
+        btn.addEventListener('mousedown', ev => {
+            ev.preventDefault();
+            this.oprimiendoEliminar = true;
+        })
+        btn.addEventListener('mouseup', ev => {
+            ev.preventDefault();
+            if (this.oprimiendoEliminar) {
+                // configurar protocolo de desaparición >:)
+                letra.addEventListener('animationend', () => {
+                    let indice = this.letrasFlotantes.indexOf(letra);
+                    this.letrasFlotantes.splice(indice, 1);
+                    letra.remove();
+                    this.oprimiendoEliminar = false;
+                })
+                // iniciar protocolo de desaparición
+                letra.classList.add('animate__zoomOut');
+            }
+        })
+        
+        // añadir boton
+        letra.appendChild(btn);
+    }
 }
+
+/* 
+Por hacer:
+
+Añadir boton mayusculas y minusculas (en el footer)
+
+Añadir sonidos (cuando de click a la letra)
+
+Añadir unirse en bloques
+
+
+*/
